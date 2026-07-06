25d57cab075cf10d0d33c60e96642e270307341f
Vash Fiery <66510785+vash-fiery@users.noreply.github.com>
2026-07-06 01:14:54 -0500

    Change API response name from 'Hono' to 'Cloudflare'

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) => c.json({ name: "Hono" }));
+app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
 
 app.get("/api/random", (c) => c.json({ number: Math.random() }));
 
