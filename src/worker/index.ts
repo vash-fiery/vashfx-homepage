112d3e4f8df17a4ff4d04c09926579b2bb05b283
Vash Fiery <66510785+vash-fiery@users.noreply.github.com>
2026-07-17 19:34:13 -0500

    Update index.ts

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -3,8 +3,4 @@ const app = new Hono<{ Bindings: Env }>();
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
 
-app.get("/api/random", (c) => c.json({ number: Math.random() }));
-
-app.get("/api/time", (c) => c.json({ time: new Date().toLocaleTimeString() }));
-
 export default app;
