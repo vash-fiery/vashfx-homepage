84f9b1342339b2880b50d5970203fe2f3da4717f
Vash Fiery <66510785+vash-fiery@users.noreply.github.com>
2026-08-15 18:18:11 -0500

    Refactor fetch method in worker/index.ts

--- worker/index.ts
+++ worker/index.ts
@@ -45,18 +45,16 @@ export default class extends WorkerEntrypoint<Env> {
           },
         });
     }
-  }
-}
+  
+    fetch(request) {
+      const url = new URL(request.url);
 
-export default {
-  fetch(request) {
-    const url = new URL(request.url);
-
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
+      if (url.pathname.startsWith("/api/")) {
+        return Response.json({
+          name: "Cloudflare",
+        });
+      }
+      return new Response(null, { status: 404 });
     }
-    return new Response(null, { status: 404 });
   },
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler<Env>
