b4ca4b7a11404ae2897df73769ab62bd8ae020ee
Vash Fiery <66510785+vash-fiery@users.noreply.github.com>
2026-08-15 15:21:02 -0500

    Update index.ts

--- worker/index.ts
+++ worker/index.ts
@@ -1,3 +1,53 @@
+import { WorkerEntrypoint } from "cloudflare:workers";
+
+export default class extends WorkerEntrypoint<Env> {
+  async fetch(request: Request) {
+    const url = new URL(request.url);
+    const key = url.pathname.slice(1);
+
+    switch (request.method) {
+      case "PUT": {
+        await this.env.R2_BUCKET.put(key, request.body, {
+          onlyIf: request.headers,
+          httpMetadata: request.headers,
+        });
+        return new Response(`Put ${key} successfully!`);
+      }
+      case "GET": {
+        const object = await this.env.R2_BUCKET.get(key, {
+          onlyIf: request.headers,
+          range: request.headers,
+        });
+
+        if (object === null) {
+          return new Response("Object Not Found", { status: 404 });
+        }
+
+        const headers = new Headers();
+        object.writeHttpMetadata(headers);
+        headers.set("etag", object.httpEtag);
+
+        // When no body is present, preconditions have failed
+        return new Response("body" in object ? object.body : undefined, {
+          status: "body" in object ? 200 : 412,
+          headers,
+        });
+      }
+      case "DELETE": {
+        await this.env.R2_BUCKET.delete(key);
+        return new Response("Deleted!");
+      }
+      default:
+        return new Response("Method Not Allowed", {
+          status: 405,
+          headers: {
+            Allow: "PUT, GET, DELETE",
+          },
+        });
+    }
+  }
+};
+
 export default {
   fetch(request) {
     const url = new URL(request.url);
