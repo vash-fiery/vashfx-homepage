a3859ed1c93e24e9fcf68329db4582410262eb96
Vash Fiery <66510785+vash-fiery@users.noreply.github.com>
2026-08-15 19:19:37 -0500

    Update index.ts

--- worker/index.ts
+++ worker/index.ts
@@ -14,14 +14,14 @@ class Handler extends WorkerEntrypoint<Env> {
 
     switch (request.method) {
       case "PUT": {
-        await this.env.R2_BUCKET.put(key, request.body, {
+        await this.env.vfx_r2.put(key, request.body, {
           onlyIf: request.headers,
           httpMetadata: request.headers,
         });
         return new Response(`Put ${key} successfully!`);
       }
       case "GET": {
-        const object = await this.env.R2_BUCKET.get(key, {
+        const object = await this.env.vfx_r2.get(key, {
           onlyIf: request.headers,
           range: request.headers,
         });
@@ -41,7 +41,7 @@ class Handler extends WorkerEntrypoint<Env> {
         });
       }
       case "DELETE": {
-        await this.env.R2_BUCKET.delete(key);
+        await this.env.vfx_r2.delete(key);
         return new Response("Deleted!");
       }
       default:
