b4cea4e202222a92d765f30420372686500f595f
Vash Fiery <66510785+vash-fiery@users.noreply.github.com>
2026-08-22 09:11:21 -0500

    Update index.ts

--- worker/index.ts
+++ worker/index.ts
@@ -1,65 +1,30 @@
 import { env } from "cloudflare:workers";
-import { WorkerEntrypoint } from "cloudflare:workers";
 
 export default {
-	async fetch(request, env) {
-		return new Response(`Hi, ${env.NAME}`);
+  async fetch(request, env) {
     const url = new URL(request.url);
-    
     if (url.pathname.startsWith("/api/")) {
-      return new Response(JSON.stringify({ name: "Cloudflare" }), {
-        headers: { "Content-Type": "application/json" },
+      return Response.json({
+        name: "Cloudflare",
       });
     }
-    // return new Response(null, { status: 404 });
     return env.ASSETS.fetch(request);
-	},
-};
+  },
 
-export default class extends WorkerEntrypoint<Env> {
-  async fetch(request: Request) {
+  async fetch(request, env) {
     const url = new URL(request.url);
     const key = url.pathname.slice(1);
-
     switch (request.method) {
-      case "PUT": {
-        await this.env.R2_VFX.put(key, request.body, {
-          onlyIf: request.headers,
-          httpMetadata: request.headers,
-        });
+      case "PUT":
+        await env.BUCKET.put(key, request.body);
         return new Response(`Put ${key} successfully!`);
-      }
-      case "GET": {
-        const object = await this.env.R2_VFX.get(key, {
-          onlyIf: request.headers,
-          range: request.headers,
-        });
-
-        if (object === null) {
-          return new Response("Object Not Found", { status: 404 });
-        }
-
-        const headers = new Headers();
-        object.writeHttpMetadata(headers);
-        headers.set("etag", object.httpEtag);
-
-        // When no body is present, preconditions have failed
-        return new Response("body" in object ? object.body : undefined, {
-          status: "body" in object ? 200 : 412,
-          headers,
-        });
-      }
-      case "DELETE": {
-        await this.env.R2_VFX.delete(key);
-        return new Response("Deleted!");
-      }
       default:
-        return new Response("Method Not Allowed", {
+        return new Response(`${request.method} is not allowed.`, {
           status: 405,
           headers: {
-            Allow: "PUT, GET, DELETE",
+            Allow: "PUT",
           },
         });
-      }
     }
-  };
+  },
+} satisfies ExportedHandler<Env>;
