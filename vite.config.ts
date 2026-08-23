3fb2d234e52cb1dc23f8535a70eeda80f0b039fe
Vash Fiery <66510785+vash-fiery@users.noreply.github.com>
2026-08-23 10:20:48 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  input: 'src/main.tsx',
   plugins: [react(), cloudflare()],
 })
